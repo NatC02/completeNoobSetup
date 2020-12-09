031505a62e6e216447dd22874f554c1e72039499
NatC02 <NatanCeballos02@gmail.com>
2020-12-09 01:25:12 -0500

    deleted duplicate

--- beginner-html-site-styled-gh-pages/scripts/main.js
+++
@@ -1,6 +0,0 @@
-const myHeading = document.querySelector('h1');
-    myHeading.textContent = 'Hello World!';
-
-    document.querySelector('html').onclick = function() {
-        alert('Ouch! Stop clicking on me!');
-    }
